93b1adb37683a06f59553578970306186e775c0b
githcorrado <githcorrado@gmail.com>
2019-07-16 21:08:18 +0800

    added 1 new TwoSum scala solution.

--- Scala_solutions/TwoSum.scala
+++ Scala_solutions/TwoSum.scala
@@ -10,7 +10,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
  */
-
+import scala.collection.mutable
+import scala.util.control.Breaks._
+/* Leetcode Evaluation Runtime around 670ms */
 object Solution {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
     val numzip = nums.zipWithIndex
@@ -20,5 +22,24 @@ object Solution {
       if ((a + nums(b_index)) == target)
     } yield (a_index, b_index)
     Array(c(0)._1, c(0)._2)
+  }
+}
+
+/* Leetcode Evaluation Runtime around 450ms */
+object Solution1 {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val numzip = nums.zipWithIndex
+    var result = mutable.ArrayBuffer[Int]()
+    breakable {
+      for ( (a, a_index) <- numzip) {
+        for (b_index <- (a_index + 1) to (nums.size - 1) ) {
+          if ( a + nums(b_index) == target)
+          {   result += a_index
+            result += b_index
+            break   }
+        }
+      }
+    }
+    result.toArray
   }
 }
