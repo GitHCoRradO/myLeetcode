fe51c874317c6d8a0f8e81af9ca75fd5322ce8bf
githcorrado <githcorrado@gmail.com>
2019-07-16 14:11:45 +0800

    added TwoSum scala solution.

---
+++ Scala_solutions/TwoSum.scala
@@ -0,0 +1,24 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+
+object Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val numzip = nums.zipWithIndex
+    val c = for {
+      (a, a_index) <- numzip
+      b_index <- (a_index + 1) to (nums.size - 1)
+      if ((a + nums(b_index)) == target)
+    } yield (a_index, b_index)
+    Array(c(0)._1, c(0)._2)
+  }
+}
